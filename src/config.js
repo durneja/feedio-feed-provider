a11a69aea66510944bcd24ccfb54f2e41ca83484
durneja <kinshuk89@gmail.com>
2022-04-20 10:19:15 +0530

    Updating response structure and including post processing
    Updating response structure and including post processing

--- src/config.js
+++ src/config.js
@@ -1,5 +1,5 @@
 module.exports = {
-    exchangeKeys: [
+    currencyKeys: [
         'BTC',
         'ETH',
         'NEO',
--- src/services/feed.service.js
+++ src/services/feed.service.js
@@ -2,7 +2,7 @@ const httpStatus = require('http-status');
 const { User } = require('../models');
 const ApiError = require('../utils/ApiError');
 const axios = require('axios');
-const { exchangeKeys } = require('../config');
+const { currencyKeys } = require('../config');
 
 /**
  * Query for data
@@ -15,15 +15,25 @@ const getData = async () => {
 
   const data = await Promise.all([binancePromise, coinMarketPromise, coinGeckoPromise]);
 
-  return {"response": data};
+  return {"response": postProcess(data)};
 };
 
 const postProcess = (data) => {
-  var result = [];
-  data.forEach((arr) => {})
-  // const dataLen = data.length;
-  // for(var i=0; i < dataLen; i++)
-  //  result.push((data[0][i] + data[1][i] + data[2][i]) / 3);
+
+  let result = {};
+  data.forEach(el => {
+    for (const property in el) {
+      if(result.hasOwnProperty(property)){
+        result[property] = result[property] + el[property];
+      } else {
+        result[property] = el[property];
+      }
+    }
+  });
+
+  for (const property in result) {
+    result[property] = result[property] / 3;
+  }
 
   return result;
 }
@@ -54,16 +64,15 @@ const fetchBinanceData = () => {
 
 const processBinanceResponse = (data) => {
   // console.log('binance data --', data)
-
-  let values = [];
+  let values = {};
   data.forEach((el) => {
-    exchangeKeys.forEach((key) => {
+    currencyKeys.forEach((key) => {
       if (el.symbol === exchangeInfoMap.binance[key]) {
-        values.push({[key]: parseFloat(el.price)})
+        values[key] = parseFloat(el.price);
       }
     })
   })
-  values.find(val => Object.keys(val)[0] === exchangeKeys[3])[exchangeKeys[3]] = values.find(val => Object.keys(val)[0] === exchangeKeys[3])[exchangeKeys[3]] * values.find(val => Object.keys(val)[0] === exchangeKeys[0])[exchangeKeys[0]]
+  values[currencyKeys[3]] = values[currencyKeys[3]] * values[currencyKeys[0]];
   // console.log('binance final values--', values);
   return values;
 }
@@ -100,12 +109,12 @@ const fetchCoinMarketcapData = () => {
 }
 const processCoinMarketcapResponse = (data) => {
   // console.log('coinmarketcap data--', data)
-  let values = [];
+  let values = {};
   // handling CoinMarketCap Object response
   Object.keys(data).forEach((el) => {
-    exchangeKeys.forEach((key) => {
+    currencyKeys.forEach((key) => {
       if (el === exchangeInfoMap.coinmarketcap[key]) {
-        values.push({[key]: data[exchangeInfoMap.coinmarketcap[key]]?.quote.USD.price})
+        values[key] = data[exchangeInfoMap.coinmarketcap[key]]?.quote.USD.price;
       }
     })
   })
@@ -140,11 +149,11 @@ const fetchCoinGeckoData = () => {
 const processCoinGeckoResponse = (data) => {
   // console.log('coingecko data--', data);
 
-  let values = [];
+  let values = {};
   data.forEach((el) => {
-    exchangeKeys.forEach((key) => {
+    currencyKeys.forEach((key) => {
       if (el.id === exchangeInfoMap.coingecko[key]) {
-        values.push({[key]: el.current_price})
+        values[key] = el.current_price;
       }
     })
   })
